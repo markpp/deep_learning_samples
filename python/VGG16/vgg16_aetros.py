12575685871ed0db4b5457de11c48f6bb477e270
mpph <mpph@create.aau.dk>
2017-02-02 13:18:04 +0100

    config system

--- python/VGG16/vgg16_aetros.py
+++ python/VGG16/vgg16_aetros.py
@@ -12,10 +12,16 @@ from keras.layers.normalization import BatchNormalization
 from keras import regularizers
 from keras import backend as K
 from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+#from aetros.keras import KerasIntegration
 
 from sklearn.metrics import log_loss, accuracy_score, confusion_matrix
 
-def vgg_std16_model(img_rows, img_cols, channel=1, num_class=None):
+import os
+import json
+#os.environ['API_KEY'] = "f7560908f0a18d5aa14c0583bc1a2f89"
+
+def vgg_std16_model(config):
     """
     VGG 16 Model for Keras
 
@@ -31,7 +37,7 @@ def vgg_std16_model(img_rows, img_cols, channel=1, num_class=None):
       num_class - number of class labels for our classification task
     """
     feature_layers = [
-        ZeroPadding2D((1, 1), input_shape=(channel, img_rows, img_cols)),
+        ZeroPadding2D((1, 1), input_shape=(config['n_channel'], config['n_rows'], config['n_cols'])),
         Convolution2D(64, 3, 3, activation='relu'),
         ZeroPadding2D((1, 1)),
         Convolution2D(64, 3, 3, activation='relu'),
@@ -82,13 +88,13 @@ def vgg_std16_model(img_rows, img_cols, channel=1, num_class=None):
     for l in feature_layers:
         l.trainable = False
     # Loads ImageNet pre-trained data
-    model.load_weights('C:/Users/maph/Documents/github/models/full/vgg16_weights.h5')
+    model.load_weights(config['pre_weight_path'])
 
     # Truncate and replace softmax layer for transfer learning
     model.layers.pop()
-    model.outputs = [model.layers[-1].output]
-    model.layers[-1].outbound_nodes = []
-    model.add(Dense(num_class, activation='softmax'))
+    #model.outputs = [model.layers[-1].output]
+    #model.layers[-1].outbound_nodes = []
+    model.add(Dense(config['n_class'], activation='softmax'))
 
     # Uncomment below to set the first 10 layers to non-trainable (weights will not be updated)
     #for layer in model.layers[:10]:
@@ -101,20 +107,10 @@ def vgg_std16_model(img_rows, img_cols, channel=1, num_class=None):
     return model
 
 if __name__ == '__main__':
+    config_file = '../../config/rog_setup.json'
+    config = json.load(open(config_file))
 
-    # Fine-tune Example
-    img_rows, img_cols = 224, 224 # Resolution of inputs
-    channel = 3
-    num_class = 2
-    batch_size = 32
-    nb_epoch = 30
-
-    train_data_dir = 'data/train'
-    validation_data_dir = 'data/validation'
-    nb_train_samples = 2000
-    nb_validation_samples = 800
-    # Load our model
-    model = vgg_std16_model(img_rows, img_cols, channel, num_class)
+    model = vgg_std16_model(config)
 
     # prepare data augmentation configuration
     train_datagen = ImageDataGenerator(
@@ -126,24 +122,32 @@ if __name__ == '__main__':
     test_datagen = ImageDataGenerator(rescale=1./255)
 
     train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_height, img_width),
-        batch_size=32,
-        class_mode='binary')
+        config['train_dir'],
+        target_size=(config['n_rows'], config['n_cols']),
+        batch_size=config['batch_size'],
+        class_mode='categorical')
 
     validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_height, img_width),
-        batch_size=32,
-        class_mode='binary')
+        config['val_dir'],
+        target_size=(config['n_rows'], config['n_cols']),
+        batch_size=config['batch_size'],
+        class_mode='categorical')
+
+    #KerasIntegration('markpp/test1', 'f7560908f0a18d5aa14c0583bc1a2f89', model, insights = True )
 
     # fine-tune the model
     model.fit_generator(
         train_generator,
-        samples_per_epoch=nb_train_samples,
-        nb_epoch=nb_epoch,
+        samples_per_epoch=config['n_train_samples'],
+        nb_epoch=config['n_epoch'],
         validation_data=validation_generator,
-        nb_val_samples=nb_validation_samples)
+        nb_val_samples=config['n_validation_samples'])
+
+    model.save_weights(config['output_weight_path'])
+
+    model_json = model.to_json()
+    with open(config['output_model_path'], "w") as json_file:
+        json_file.write(model_json)
     '''
     train_datagen = ImageDataGenerator(
         rescale=1./255,
